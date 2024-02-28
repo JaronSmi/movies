1438b62c58749e2e9545bb7a39135feeac5db689
JaronSmi <smithjaron5@gmail.com>
2024-02-28 15:50:13 -0600

    Resolve Fixnum bug, use interpolation for vars

--- movies.rb
+++ movies.rb
@@ -1,3 +1,15 @@
+# puts 'Jaron loves Starwars'
+# puts 'Jaron\'s favorite movie is Starwars'
+
+# movie = "Lego movie"
+# puts 'Jaron\'s favorite movie is ' + movie
+
+# rank = 10
+
+# puts movie + ' has a rank of ' + rank
+
+
+#fix Fixnum to String conversion
 puts 'Jaron loves Starwars'
 puts 'Jaron\'s favorite movie is Starwars'
 
@@ -6,4 +18,15 @@ puts 'Jaron\'s favorite movie is ' + movie
 
 rank = 10
 
-puts movie + ' has a rank of ' + rank
+puts movie + ' has a rank of ' + rank.to_s
+
+# Use "" instead of '', use interpolation
+puts "Jaron loves Starwars"
+puts "Jaron's favorite movie is Starwars"
+
+movie = "Lego movie"
+puts "Jaron's favorite movie is #{movie}"
+
+rank = 10
+
+puts "#{movie} has a rank of #{rank}"
