a8a639be18ae0e551f03fb82c231cff0a355f3db
JaronSmi <smithjaron5@gmail.com>
2024-02-28 15:43:15 -0600

    Fixnum to String error

---
+++ movies.rb
@@ -0,0 +1,9 @@
+puts 'Jaron loves Starwars'
+puts 'Jaron\'s favorite movie is Starwars'
+
+movie = "Lego movie"
+puts 'Jaron\'s favorite movie is ' + movie
+
+rank = 10
+
+puts movie + ' has a rank of ' + rank
